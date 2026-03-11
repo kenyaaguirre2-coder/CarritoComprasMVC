a415c40b9a219e4a7c786956fea8b26aca30310e
Kenya <Kenya@KENYA>
2026-03-11 15:29:20 -0700

    Added class Program.cs to folder Models (Models/Program.cs)

---
+++ Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿// Models/Producto.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace CarritoComprasMVC.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del producto es obligatorio")]
+        [Display(Name = "Nombre del Producto")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 100 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        [Display(Name = "Precio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+        [DataType(DataType.Currency)]
+        public decimal Precio { get; set; }
+    }
+}
