96bfb8adf8523999d8c00e1d4096f43cce0f2117
Kenya <Kenya@KENYA>
2026-03-11 15:26:05 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarritoComprasMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
